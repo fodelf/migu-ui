59ae2dc8adc5d15dec88d8416323d09df9cc411d
Daniel <740907068@qq.com>
2020-12-22 09:33:24 +0800

    解决rollup不支持import时拼字符串的问题

--- src/router.ts
+++ src/router.ts
@@ -6,9 +6,12 @@ import ButtonDemo from './components/ButtonDemo/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo/TabsDemo.vue'
 import Markdown from './components/Markdown.vue'
-import { h } from 'vue'
+import intro from './markdown/intro.md'
+import install from './markdown/install.md'
+import getstarted from './markdown/get-started.md'
+import { h } from 'vue' 
 const history=createWebHashHistory()
-const md=filename=>h(Markdown,{path:`../markdown/${filename}.md`,key:filename})
+const md=string=>h(Markdown,{content:string,key:string})
 export const router=createRouter({
     history:history,
     routes:[
@@ -26,15 +29,15 @@ export const router=createRouter({
                 },
                 {
                     path:"intro",
-                    component:md('intro')
+                    component:md(intro)
                 },
                 {
                     path:"install",
-                    component:md('install')
+                    component:md(install)
                 },
                 {
                     path:"get-started",
-                    component:md('get-started')
+                    component:md(getstarted)
                 },
                 {
                     path:'switch',
